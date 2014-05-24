03cae811a7a20c5897d19844c2698fdb9a475296
Arata <tarata43@yahoo.co.jp>
2014-05-24 18:23:51 +0900

    細かいバグ修正

--- scripts/elastic-search-install.sh
+++ scripts/elastic-search-install.sh
@@ -13,5 +13,13 @@ if [ -e /opt/elasticsearch ]; then
 else
     mv -f /tmp/elasticsearch-1.0.1 /opt/elasticsearch
     cat /vagrant/scripts/elasticsearch/config.yml >> /opt/elasticsearch/config/elasticsearch.yml
+    # plugins for japanese
+    # http://qiita.com/hilotter/items/b3038be41605775bcf69
+    /opt/elasticsearch/bin/plugin --install elasticsearch/elasticsearch-analysis-kuromoji/1.5.0
+    curl -L http://github.com/elasticsearch/elasticsearch-servicewrapper/tarball/master | tar -xz
+    mv *servicewrapper*/service /opt/elasticsearch/bin/
+    rm -Rf *servicewrapper*
+    /opt/elasticsearch/bin/service/elasticsearch install
 fi
-/opt/elasticsearch/bin/elasticsearch -d
+
+service elasticsearch start
